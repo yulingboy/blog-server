94577ba401e84efdfe48bb07dc2c5e7155cfc7e4
yulingboy <yulinglsy@163.com>
2020-09-08 17:44:17 +0800

    第二次提交

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ const app = express();
 app.use(bodyPaser.json());
 // 处理post请求参数
 app.use(bodyPaser.urlencoded({ extended: false }));
+//设置跨域请求
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 app.use(cors());
 // 将 JWT 字符串还原为 JSON 对象 
 app.use(expressJWT({ secret: secretKey, algorithms: ['HS256'] }).unless({
